5fd5a1d97b891e7001f619d19e227d563fc37096
Andres Belfanti <54213335+andresbelfanti@users.noreply.github.com>
2024-06-22 21:20:23 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -28,9 +28,9 @@ var APP = {
 			if ( project.toneMapping !== undefined ) renderer.toneMapping = project.toneMapping;
 			if ( project.toneMappingExposure !== undefined ) renderer.toneMappingExposure = project.toneMappingExposure;
 
-			this.setCamera( loader.parse( json.camera ) ); // estos elementos nuevos
-			var controls = new OrbitControls( camera, renderer.domElement ); // este tambien
-			
+			this.setScene( loader.parse( json.scene ) );
+			this.setCamera( loader.parse( json.camera ) );
+
 			events = {
 				init: [],
 				start: [],
