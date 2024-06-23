6a1c4f3db13413bdf1234664f3e24753c6078ec9
Andres Belfanti <54213335+andresbelfanti@users.noreply.github.com>
2024-06-22 21:20:59 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -30,6 +30,7 @@ var APP = {
 
 			this.setScene( loader.parse( json.scene ) );
 			this.setCamera( loader.parse( json.camera ) );
+			var controls = new OrbitControls( camera, renderer.domElement );
 
 			events = {
 				init: [],
